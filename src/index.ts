a711083e2c222c97a72a5e689f0ba2139399baf1
pitch.ktp <pitch@odds.team>
2025-12-21 22:37:01 +0700

    Update dependencies and configure CORS middleware
    - Added CORS support to the Express application for handling cross-origin requests.
    - Updated package.json and package-lock.json to include new dependencies such as @types/cors and jest.
    - Enhanced the package-lock.json with additional dependencies and updated versions for better compatibility.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
